6d34673c2eb5f6ac28bf2f00b202e87bb36c2286
marial6750 <75641837+marial6750@users.noreply.github.com>
2020-12-14 20:34:34 -0600

    Update main.py

--- MATH-2305-Final-Project/main.py
+++ MATH-2305-Final-Project/main.py
@@ -9,5 +9,23 @@ from drawing import *
 graph_data = open('test-graph/G2.txt', 'r')
 
 G = nx.read_weighted_edgelist(graph_data, nodetype= int)
+"""
+Parameters 
+---------
+graph_data  : boolean
+
+nodetype : int
+
+"""
 
 T= prims_algorithm(G, 3, show_graph = True, show_cost = True)
+"""
+Parameters
+----------
+G : int
+
+show_graph : boolean
+
+show_cost : boolean
+
+"""
