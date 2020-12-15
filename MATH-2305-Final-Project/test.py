ff37b8ed7549b57093e101fafdce41f49a8da113
Narph79 <47196221+Narph79@users.noreply.github.com>
2020-12-15 14:14:17 -0600

    Update test.py

--- MATH-2305-Final-Project/test.py
+++ MATH-2305-Final-Project/test.py
@@ -1,7 +1,7 @@
 import networkx as nx
 from functions import *
 from algorithms import prims_algorithm
-
+#Test.py holds all the functions that are tested before being incorporated into main, functions, etc.
 graph_data = open('test-graph/G2.txt', 'r')
 
 G = nx.read_weighted_edgelist(graph_data, nodetype= int)
